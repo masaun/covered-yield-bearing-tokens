635fd7e18af170c32321a7c55c90208f07a5030b
masaun <masanoriuno@gmail.com>
2020-11-27 11:07:14 +0900

    Define claim() and redeemClaimWithFund() in the Distributor.sol

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -11,6 +11,21 @@ contract Distributor {
 
     constructor() public {}
 
+    /***
+     * @notice - Claims can be submitted via the distributor contract by returning the NFT.
+     *           (this creates a claim assessment item for Nexus Claims Assessors who then vote on claims)
+     **/
+    function claim() public returns (bool) {
+        redeemClaimWithFund();
+    }
+
+
+    /***
+     * @notice - Assuming the claim is paid the funds are transferred to the Distributor and can be redeemed by the end user.
+     **/
+    function redeemClaimWithFund() internal returns (bool) {}
+    
+
 
     ///------------------------------------------------------------
     /// Internal functions
